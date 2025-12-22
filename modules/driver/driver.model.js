836ec38074687452db17f6a4fa1addfc01dc2c9b
SunithaSivakumaran <SunithaSivakumaran654@gmail.com>
2025-12-22 13:17:28 +0530

    adding driver model without validation

---
+++ modules/driver/driver.model.js
@@ -0,0 +1,24 @@
+
+import mongoose from "mongoose";
+
+export const driverSchema = new mongoose.Schema({
+ name: {
+  type: String,
+  required: true,
+ },
+ phone: {
+  type: String,
+  match: /^\d{10}$/,
+  required: true,
+ },
+ status: {
+  type: String,
+  enum: ["available", "unavailable", "inService","onDuty"],
+  default: "unavailable",
+ },
+ license_no: {
+  type: String,
+  required: true,
+  //validation should be here
+ },
+});
