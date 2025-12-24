04ad847101e65be99d3355abf4e44eb9a3f09876
SunithaSivakumaran <SunithaSivakumaran654@gmail.com>
2025-12-24 09:36:39 +0530

    after fiishing model

--- modules/config/config.model.js
+++ modules/config/config.model.js
@@ -1,9 +1,34 @@
+import { min } from "lodash";
 import mongoose from "mongoose";
+
+const FIXED_TEMPLET="Your milk production of [VOLUME] liters is scheduled for pickup."
+
+const lat_fat_tableSchema = new mongoose.Schema({
+  lat: { type: [Number], required: true },
+  fat: { type: [Number], required: true },
+  rates: { type: [[Number]], required: true },
+})
+
 
 const configSchema = new mongoose.Schema({
  deport_location: {
   lat: { type: Number, required: true },
   lon: { type: Number, required: true },
  },
- 
-});
+ notification_template: {
+  templet: { type: String,
+  enum: [FIXED_TEMPLET],
+  required: true},
+  volume: { type: Number,min: 0,
+  required: true},
+},
+ lat_fat_table: {
+  type: lat_fat_tableSchema,
+  required: true,
+ },
+
+
+}, { timestamps: true });
+
+ const Config = mongoose.model("Config", configSchema);
+export default Config
