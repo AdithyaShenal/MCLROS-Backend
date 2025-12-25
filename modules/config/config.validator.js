4f665079f37439b9a23777e03b2ae28a1898e574
SunithaSivakumaran <SunithaSivakumaran654@gmail.com>
2025-12-25 09:58:39 +0530

    before testing config

--- modules/config/config.validator.js
+++ modules/config/config.validator.js
@@ -1,21 +1,21 @@
 import joi from 'joi'
 
 export const createConfigSchema = joi.object({
-deport_location: joi.object({ lat: joi.number().required(), lon: joi.number().required() }).unique().required()
+deport_location: joi.object({ lat: joi.number().required(), lon: joi.number().required() }).unique().unknown(false),
 })
 
 export const configTemplateSchema = joi.object({
- deport_location: joi.object({ lat: joi.number().required(), lon: joi.number().required() }).unique().required(),
+ deport_location: joi.object({ lat: joi.number().required(), lon: joi.number().required() }).unknown(false),
   notification_template: joi.string().required().pattern(/\d/),
 })
 
 export const configLat_Fat_TableSchema = joi.object({
-  deport_location: joi.object({ lat: joi.number().required(), lon: joi.number().required() }.required()),
+  deport_location: joi.object({ lat: joi.number().required(), lon: joi.number().required() }.unknown(false)),
   lat_fat_table: joi.object({
-   lat: joi.array().number(),
-   fat: joi.array().numer(),
-   rates: joi.array().number(),
-  }).required(),
+   lat: joi.array().number().required(),
+   fat: joi.array().numer().required(),
+   rates: joi.array().number().required(),
+  }).unknown(false),
 })
 
 
