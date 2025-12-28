0b82e814970250ce7a862a590a4ac0f1da758bd0
Adithya Shenal <washenal55@gmail.com>
2025-12-29 00:14:47 +0530

    cors modified

--- index.js
+++ index.js
@@ -37,7 +37,11 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "capacitor://localhost",
+      "http://localhost",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
