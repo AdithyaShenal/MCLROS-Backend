30efd09967d9305b61a98ac7a558016a1286538d
Adithya Shenal <washenal55@gmail.com>
2025-12-31 11:09:29 +0530

    Allowed new origin in CORS

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5175",
+      "http://localhost:5174",
       "http://localhost:5173",
       "http://localhost",
       "capacitor://localhost",
