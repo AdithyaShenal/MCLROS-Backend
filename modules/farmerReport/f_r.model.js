663b5d179cfe684747d62cca50dcbeef97c26948
SunithaSivakumaran <SunithaSivakumaran654@gmail.com>
2025-12-31 10:13:38 +0530

    after finishing the model in farmerReport

--- modules/farmerReport/f_r.model.js
+++ modules/farmerReport/f_r.model.js
@@ -1,5 +1,36 @@
-import monggoose from "mongoose";
+import mongoose from 'mongoose'
 
-const farmerReportSchema = new monggoose.Schema({
- 
-});
+const farmerReportSchema = new monggoose.Schema(
+  {
+    farmerID: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'farmer',
+      require: true,
+    },
+    report: {
+      type: String,
+      require: true,
+    },
+    adminID: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'admin',
+      require: true,
+    },
+    feedback: {
+      type: String,
+    },
+    reply_time: {
+      type: mongoose.Schema.Types.Date,
+    },
+    status: {
+      type: String,
+      enum: ['pending', 'resolved'],
+      default: 'pending',
+    },
+  },
+  { timestamps: true }
+)
+
+const FarmerReport = mongoose.model('FarmerReport', farmerReportSchema)
+
+export default FarmerReport
