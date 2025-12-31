be6cb35999869aa2e02d49fa053b1559de4604f5
SunithaSivakumaran <SunithaSivakumaran654@gmail.com>
2025-12-31 10:44:43 +0530

    after creating some functions in repo

--- modules/farmerReport/f_r.repository.js
+++ modules/farmerReport/f_r.repository.js
@@ -1,6 +1,18 @@
-import FarmerReport from "./f_r.model";
+import FarmerReport from './f_r.model.js'
 
 export async function create(data) {
- const farmer_report=new FarmerReport(data)
- return await farmer_report.save();
+  const farmer_report = new FarmerReport(data)
+  return await farmer_report.save()
 }
+
+export async function delete_report(id) {
+  return await FarmerReport.findByIdAndDelete(id)
+}
+
+
+
+export async function get_report(id) {
+  return await FarmerReport.findById(id).populate("farmerID","name phone").populate("adminID","name")
+}
+
+
