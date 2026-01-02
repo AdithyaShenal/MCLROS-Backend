fbd4b539682fbc34c613b71096aa51cd3555ec00
Adithya Shenal <washenal55@gmail.com>
2026-01-02 18:49:31 +0530

    driver login auth implemented

--- modules/driver/driver.service.js
+++ modules/driver/driver.service.js
@@ -1,43 +1,69 @@
 import * as driverRepository from "./driver.repository.js";
 import * as errors from "../../errors/errors.js";
+import Driver from "./driver.model.js";
 
-export async function findAll(){
- const drivers = await driverRepository.findAll();
- if(!drivers || drivers.length === 0) throw new  errors.NotFoundError('Drivers not found');
- return drivers;
+// Generate a random 4 digit pin
+function generatePin() {
+  return Math.floor(1000 + Math.random() * 9000).toString();
 }
+
+export async function findAll() {
+  const drivers = await driverRepository.findAll();
+  if (!drivers || drivers.length === 0)
+    throw new errors.NotFoundError("Drivers not found");
+  return drivers;
+}
+
 export async function createDriver(data) {
- const driver = await driverRepository.findDriverByLicenseNo(data.driver_license_no); //check if driver already exists()
- if(driver) throw new errors.BadRequestError("Driver already exists");
- return await driverRepository.create(data);
+  const driver = await driverRepository.findDriverByLicenseNo(
+    data.driver_license_no
+  );
+
+  if (driver) throw new errors.BadRequestError("Driver already exists");
+
+  let pin;
+  let exists = true;
+
+  while (exists) {
+    pin = generatePin();
+    exists = await Driver.exists({ pinNo: pin });
+  }
+
+  const newDriver = { ...data, pinNo: pin };
+
+  return await driverRepository.create(newDriver);
 }
 
-export async  function updateDriver(id, data) {
- const driver = await driverRepository.findDriverById(id);
- if(!driver) throw new errors.NotFoundError("Driver not found");
- return await driverRepository.update(id, data);
+export async function updateDriver(id, data) {
+  const driver = await driverRepository.findDriverById(id);
+  if (!driver) throw new errors.NotFoundError("Driver not found");
+  return await driverRepository.update(id, data);
 }
 
 export async function deleteDriver(id) {
- const driver = await driverRepository.findDriverById(id);
- if(!driver) throw new errors.NotFoundError("Driver not found");
- return await driverRepository.delete_driver(id);
+  const driver = await driverRepository.findDriverById(id);
+  if (!driver) throw new errors.NotFoundError("Driver not found");
+  return await driverRepository.delete_driver(id);
 }
 
 export async function getDriverById(id) {
- const driver = await driverRepository.findDriverById(id);
- if(!driver) throw new  errors.NotFoundError("Driver not found");
- return driver;
+  const driver = await driverRepository.findDriverById(id);
+  if (!driver) throw new errors.NotFoundError("Driver not found");
+  return driver;
 }
 
 export async function getDriverByLicenseNo(driver_license_no) {
- const driver = await driverRepository.findDriverByLicenseNo(driver_license_no);
- if(!driver) throw new errors.NotFoundError("Driver not found");
- return driver;
+  const driver = await driverRepository.findDriverByLicenseNo(
+    driver_license_no
+  );
+  if (!driver) throw new errors.NotFoundError("Driver not found");
+  return driver;
 }
 
 export async function toggleDriverStatus(driver_license_no, status) {
- const driver = await driverRepository.findDriverByLicenseNo(driver_license_no);
- if(!driver) throw new  errors.NotFoundError("Driver not found");
- return await driverRepository.toggleStatus(driver_license_no, status);
+  const driver = await driverRepository.findDriverByLicenseNo(
+    driver_license_no
+  );
+  if (!driver) throw new errors.NotFoundError("Driver not found");
+  return await driverRepository.toggleStatus(driver_license_no, status);
 }
--- modules/driver/driver.validator.js
+++ modules/driver/driver.validator.js
@@ -1,35 +1,35 @@
 import joi from "joi";
 
-
 export const driverIdSchema = joi.object({
   id: joi.string().required(),
 });
 
 export const createDriverSchema = joi.object({
- name: joi.string().required(),
- phone: joi.string().pattern(/^\d{10}$/),
- driver_license_no: joi.string().required(),
- status: joi.string().valid("available", "unavailable","onDuty"),
+  name: joi.string().required(),
+  phone: joi.string().pattern(/^\d{10}$/),
+  driver_license_no: joi.string().required(),
+  status: joi.string().valid("available", "unavailable", "onDuty").optional(),
+  shortName: joi.string().required(),
 });
 
 export const updateDriverSchema = joi.object({
- name: joi.string(),
- phone: joi.string().pattern(/^\d{10}$/),
- driver_license_no: joi.string().required(),
- status: joi.string().valid("available", "unavailable","onDuty"),
+  name: joi.string(),
+  phone: joi.string().pattern(/^\d{10}$/),
+  driver_license_no: joi.string().required(),
+  status: joi.string().valid("available", "unavailable", "onDuty"),
 });
 
 export const toggleStatusSchema = joi.object({
   driver_license_no: joi.string().required(),
-  status: joi.string().valid("available", "unavailable","onDuty").required(),
- 
+  status: joi.string().valid("available", "unavailable", "onDuty").required(),
 });
 
 // middleware/validate.js
 export const bodyValidator = (schema) => {
   return (req, res, next) => {
     const { error } = schema.validate(req.body);
-    if (error) return res.status(400).json({ message: error.details[0].message });
+    if (error)
+      return res.status(400).json({ message: error.details[0].message });
     next();
   };
 };
@@ -37,7 +37,8 @@ export const bodyValidator = (schema) => {
 export const paramsValidator = (schema) => {
   return (req, res, next) => {
     const { error } = schema.validate(req.params);
-    if (error) return res.status(400).json({ message: error.details[0].message });
+    if (error)
+      return res.status(400).json({ message: error.details[0].message });
     next();
   };
 };
